261ec8ffd3a00cd1ed950ab22e46f55c2970ca7d
Michael Rafael de Andrade <realteceinformatica@gmail.com>
2024-07-14 11:45:44 -0300

    resolvendo o exercicio 01 - Array - DevSuperior

---
+++ src/application/Program.java
@@ -0,0 +1,22 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// Programa para ler um número inteiro N (máximo = 10)
+		// Mostrar na tela os números negativos lidos.
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Quantos numeros voce vai digitar? ");
+		
+		
+		
+		sc.close();
+	}
+
+}
