e72ae643812c39a9457aa1c272a84a28d88a77f5
Michael Rafael de Andrade <realteceinformatica@gmail.com>
2024-07-14 11:52:29 -0300

    resolvido

--- src/application/Program.java
+++ src/application/Program.java
@@ -13,9 +13,23 @@ public class Program {
 		Scanner sc = new Scanner(System.in);
 		
 		System.out.print("Quantos numeros voce vai digitar? ");
+		int numero = sc.nextInt();
+		int[] vect = new int[numero];
 		
+		for(int i = 0; i < numero; i++) {
+			System.out.print("Digite um numero: ");
+			vect[i] = sc.nextInt();
+		}
 		
+		System.out.println("NUMEROS NEGATIVOS: ");
 		
+		for(int i = 0; i < numero; i++) {
+	
+			if(vect[i] < 0) {
+				System.out.println(vect[i]);
+			}
+		}
+			
 		sc.close();
 	}
 
